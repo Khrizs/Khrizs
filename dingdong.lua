eb4ebdff01e6c7af482db2e2ce12fbc5aceec47e
Khrizz <98967600+Khrizs@users.noreply.github.com>
2025-10-16 12:29:16 -0400

    Delete dingdong.lua

--- dingdong.lua
+++
@@ -1,9 +0,0 @@
-script.parent.parent.Speaker.s1 = StopRequest
-script.parent.parent.UpStop = InSign
-
-script.parent.clickDetector.MouseClick:connect(clicked)
-
-function clicked()
-  StopRequest:Play()
-  InSign.SurfaceGui.TextLabel.Text = Bus Stopping
-end
